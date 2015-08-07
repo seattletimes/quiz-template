fb8eaf399b399cfe4e90f68b0c8f8012952b53c1
Audrey Carlsen <acarlsen@seattletimes.com>
2015-08-07 16:23:27 -0700

    add new image subfolders

--- template.js
+++ template.js
@@ -23,6 +23,9 @@ exports.template = function(grunt, init, done) {
     var root = init.filesToCopy(props);
     init.copyAndProcess(root, props, {noProcess: "src/assets/**"});
     grunt.file.mkdir("data");
+    grunt.file.mkdir("./src/assets/quiz");
+    grunt.file.mkdir("./src/assets/results");
+    grunt.file.mkdir("./src/assets/elsewhere");
 
     //install node modules
     console.log("Installing Node modules...");
